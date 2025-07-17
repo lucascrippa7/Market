5876420912823ac1c76871cbd87ebc8182039923
LucasCrippa <lucas_crippa@hotmail.com>
2025-07-17 17:40:58 -0300

    Teste Produto findAll

---
+++ src/test/java/br/com/LucasCrippa/Market/services/ProdutoServiceTest.java
@@ -0,0 +1,52 @@
+package br.com.LucasCrippa.Market.services;
+
+import br.com.LucasCrippa.Market.entities.Produto;
+import br.com.LucasCrippa.Market.entities.enums.TipoProduto;
+import br.com.LucasCrippa.Market.repositories.ProdutoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ProdutoServiceTest {
+
+
+    @Mock
+    private ProdutoRepository produtoRepository;
+
+    @InjectMocks
+     private  ProdutoService produtoService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    @Test
+    void findAllRetornaListaProdutos() {
+
+        Produto produto = new Produto(1L, "Perfume", TipoProduto.PERFUME, 12.56);
+        Produto produto2 = new Produto(2L, "Shampoo", TipoProduto.PRODUTOSCAPILARES, 15.00);
+
+        when(produtoRepository.findAll()).thenReturn(Arrays.asList(produto, produto2));
+
+        List<Produto> produtos = produtoService.findAll();
+
+        assertEquals(2, produtos.size());
+        verify(produtoRepository, times(1)).findAll();
+
+    }
+
+    @Test
+    void findById() {
+    }
+}
